0647ccdfab5427272e08f35f86fa4d49b36eef69
Kevin <kevin.ntcse211@gmail.com>
2022-06-13 20:46:15 +0800

    test08

--- dedata.py
+++ dedata.py
@@ -1,17 +1,15 @@
-import requests , json
-import firebase_admin
-import zlib
+import zlib ,datetime ,firebase_admin
 from firebase_admin import credentials ,firestore
 
 cred = credentials.Certificate('testkey.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 collection_ref = db.collection("test")
-
+today = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8)))
 
 asd= db.collection("test").document("key")
 
-asdf = asd.get().to_dict()['2022-06-13']
+asdf = asd.get().to_dict()[str(today.date())]
 # asd = zlib.compress(asdf.decode("utf-8"))
 # .decode('uft-8')  
 asdfg = zlib.decompress(asdf).decode("utf-8")
